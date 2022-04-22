2c93275a6797cef3af4aaa9d00441f489c0d5c75
Ronalmoo <ydaniel0826@gmail.com>
2022-04-22 11:51:01 +0900

    feat: convert from RDD to DataFrame

--- dps/spark/jobs/sample_jsonl.py
+++ dps/spark/jobs/sample_jsonl.py
@@ -1,7 +1,8 @@
 import yaml
-
+import json
 from pyspark import SparkContext
 from pyspark.rdd import RDD
+from pyspark.sql import DataFrame
 
 from ..spark_session import spark_session
 from ..utils.io import read_line, to_json
@@ -13,15 +14,12 @@ def sample_jsonl(config_path):
     
     input_paths = ','.join([f'{conf["base_dir"]}/{t}' for t in conf["targets"]])
 
-    with spark_session(f'sample_jsonl_size_{conf["sample_ratio"]}_seed_{conf["seed"]}') as spark:
-        sc: SparkContext = spark.sparkContext
+    with spark_session(f'sample_jsonl_size_{conf["sample_ratio"]}_seed_{conf["seed"]}') as spark:    
+        df: DataFrame = spark.read.json(input_paths).repartition(10)
+        df_json = df.distinct().repartition(1)
+        # df_json = df_json.toJSON().map(lambda x: json.loads(x)).collect()
+        # print(type(df_json))
+        # with open('./datasets/test_output_data', 'w') as f:
+        # json.dumps(df_json, indent=4).write.option("multiLine", "true").json('./datasets/test_output_data')
 
-        proc_rdd: RDD = sc.textFile(input_paths) \
-            .repartition(10) \
-            .flatMap(read_line) \
-            .sample(False, conf["sample_ratio"], conf["seed"])
-            
-        proc_rdd \
-            .repartition(1) \
-            .flatMap(to_json) \
-            .saveAsTextFile(conf["output_dir"])
+        df_json.write.option("delimiter", "\n").json('./datasets/test_output_data')
