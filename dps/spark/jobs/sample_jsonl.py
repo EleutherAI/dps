67c9b4c8d99cd486bda387c0cca69f8940b9abae
Ronalmoo <ydaniel0826@gmail.com>
2022-04-25 17:06:05 +0900

    feat: Add preprocessing methods(#1)

--- dps/spark/jobs/sample_jsonl.py
+++ dps/spark/jobs/sample_jsonl.py
@@ -1,11 +1,11 @@
 import yaml
 import json
 from pyspark import SparkContext
-from pyspark.rdd import RDD
 from pyspark.sql import DataFrame
 
 from ..spark_session import spark_session
-from ..utils.io import read_line, to_json
+from ..utils.preprocessing import *
+from pathlib import Path
 
 
 def sample_jsonl(config_path):
@@ -17,9 +17,20 @@ def sample_jsonl(config_path):
     with spark_session(f'sample_jsonl_size_{conf["sample_ratio"]}_seed_{conf["seed"]}') as spark:    
         df: DataFrame = spark.read.json(input_paths).repartition(10)
         df_json = df.distinct().repartition(1)
-        # df_json = df_json.toJSON().map(lambda x: json.loads(x)).collect()
-        # print(type(df_json))
-        # with open('./datasets/test_output_data', 'w') as f:
-        # json.dumps(df_json, indent=4).write.option("multiLine", "true").json('./datasets/test_output_data')
 
-        df_json.write.option("delimiter", "\n").json('./datasets/test_output_data')
+        preprocess_functions = [reduce_emoticon, replace_phone_number, replace_ssn, remove_whitespace, strip_html_tags]
+        df_json = df_json.toJSON().map(lambda x: json.loads(x)).collect()
+        
+        try:
+            with open('./datasets/test_output_data/sample.jsonl', 'w') as out_f:
+                for file in df_json:
+                    # import pdb; pdb.set_trace()
+                    file['text'] = preprocess_text(file['text'], preprocess_functions)
+                    json.dump(file, out_f, ensure_ascii=False)
+                    out_f.write('\n')
+        except:
+            Path('./datasets/test_output_data/sample.jsonl').touch()
+            with open('./datasets/test_output_data/sample.jsonl', 'w') as out_f:
+                for file in df_json:
+                    json.dump(file, out_f, ensure_ascii=False)
+                    out_f.write('\n')
