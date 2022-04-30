403d6380b390cc783d50f2f2a8ca4cd9e8af9a0d
Seungmoo Yang <ydaniel0826@gmail.com>
2022-04-30 14:13:26 +0900

    feat: Add new job

--- dps/spark/run.py
+++ dps/spark/run.py
@@ -1,6 +1,10 @@
 import fire
 
 from .jobs.sample_jsonl import sample_jsonl
+from .jobs.preprocess_jsonl import preprocess_jsonl
 
 def run():
-    fire.Fire({'sample_jsonl': sample_jsonl})
+    fire.Fire({
+        'sample_jsonl': sample_jsonl,
+        'preprocess_jsonl': preprocess_jsonl,
+        })
