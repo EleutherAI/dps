630bed450d930d739cb2139299ef946eb283f016
Seungmoo Yang <ydaniel0826@gmail.com>
2022-04-30 14:12:27 +0900

    fix: Rollback to original

--- dps/spark/jobs/sample_jsonl.py
+++ dps/spark/jobs/sample_jsonl.py
@@ -1,11 +1,10 @@
 import yaml
-import json
+
 from pyspark import SparkContext
-from pyspark.sql import DataFrame
+from pyspark.rdd import RDD
 
 from ..spark_session import spark_session
-from ..utils.text_normalize import *
-from pathlib import Path
+from ..utils.io import read_line, to_json
 
 
 def sample_jsonl(config_path):
@@ -14,23 +13,15 @@ def sample_jsonl(config_path):
     
     input_paths = ','.join([f'{conf["base_dir"]}/{t}' for t in conf["targets"]])
 
-    with spark_session(f'sample_jsonl_size_{conf["sample_ratio"]}_seed_{conf["seed"]}') as spark:    
-        df: DataFrame = spark.read.json(input_paths).repartition(10)
-        df_json = df.distinct().repartition(1)
+    with spark_session(f'sample_jsonl_size_{conf["sample_ratio"]}_seed_{conf["seed"]}') as spark:
+        sc: SparkContext = spark.sparkContext
 
-        preprocess_functions = [reduce_emoticon, replace_phone_number, replace_rrn, remove_whitespace, strip_html_tags]
-        df_json = df_json.toJSON().map(lambda x: json.loads(x)).collect()
-        
-        try:
-            with open('./datasets/test_output_data/sample.jsonl', 'w') as out_f:
-                for file in df_json:
-                    # import pdb; pdb.set_trace()
-                    file['text'] = preprocess_text(file['text'], preprocess_functions)
-                    json.dump(file, out_f, ensure_ascii=False)
-                    out_f.write('\n')
-        except:
-            Path('./datasets/test_output_data/sample.jsonl').touch()
-            with open('./datasets/test_output_data/sample.jsonl', 'w') as out_f:
-                for file in df_json:
-                    json.dump(file, out_f, ensure_ascii=False)
-                    out_f.write('\n')
+        proc_rdd: RDD = sc.textFile(input_paths) \
+            .repartition(10) \
+            .flatMap(read_line) \
+            .sample(False, conf["sample_ratio"], conf["seed"])
+            
+        proc_rdd \
+            .repartition(1) \
+            .flatMap(to_json) \
+            .saveAsTextFile(conf["output_dir"])
