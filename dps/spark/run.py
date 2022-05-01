dc7c7a96169a582e6e3999b6f24cc58a73eab854
jayseok-park <genima.team@gmail.com>
2022-05-01 18:22:24 +0900

    feat: Add new job to run.py

--- dps/spark/run.py
+++ dps/spark/run.py
@@ -1,6 +1,10 @@
 import fire
 
 from .jobs.sample_jsonl import sample_jsonl
+from .jobs.massivetext_quality_filtering import massivetext_quality_filtering
+
 
 def run():
-    fire.Fire({'sample_jsonl': sample_jsonl})
+    fire.Fire({'sample_jsonl': sample_jsonl,
+               "massivetext_quality_filtering": massivetext_quality_filtering
+               })
