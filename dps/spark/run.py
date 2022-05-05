ee6c921013545da251322bcc2a160736447bdd26
Seungmoo Yang <ydaniel0826@gmail.com>
2022-05-05 10:39:11 +0900

    Change job name

--- dps/spark/run.py
+++ dps/spark/run.py
@@ -1,10 +1,10 @@
 import fire
 
 from .jobs.sample_jsonl import sample_jsonl
-from .jobs.common_preprocess_jsonl import preprocess_jsonl
+from .jobs.common_preprocess_jsonl import preprocess
 
 def run():
     fire.Fire({
         'sample_jsonl': sample_jsonl,
-        'common_preprocess_jsonl': preprocess_jsonl,
+        'common_preprocess_jsonl': preprocess,
         })
