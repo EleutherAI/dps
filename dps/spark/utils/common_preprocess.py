e4ce382a8a49afb076cbe0a3eb23413300c0e335
Taekyoon <tgchoi03@gmail.com>
2022-05-09 22:51:27 +0900

    Update common_preprocess.py

--- dps/spark/utils/common_preprocess.py
+++ dps/spark/utils/common_preprocess.py
@@ -1,24 +1,16 @@
 import re
-from unicodedata import normalize
-from typing import List, Optional, Callable
+
 from bs4 import BeautifulSoup
-
-from soynlp.normalizer import *
-
-
-def clean_text(input_text: str) -> str:
-    processed_text = re.sub('[^가-힣ㄱ-ㅎㅏ-ㅣ\\s]', " ", input_text)
-    return processed_text
+from soynlp.normalizer import emoticon_normalize
 
 
 def remove_url(input_text: str) -> str:
     return re.sub('(www|http)\\S+', '', input_text)
 
 
-def strip_html_tags(text):
+def remove_html_tags(text):
     soup = BeautifulSoup(text, "html.parser")
-    stripped_text = soup.get_text(separator=" ")
-    return stripped_text
+    return soup.get_text(separator=" ")
 
 
 def remove_whitespace(input_text: str, remove_duplicate_whitespace: bool = True) -> str:
@@ -34,13 +26,11 @@ def replace_email(input_text: str) -> str:
 
 def replace_phone_number(input_text: str) -> str:
     regex_pattern = '[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}'    
-
     return re.sub(regex_pattern, '<|tel|>', input_text)
 
 
 def replace_rrn(input_text: str) -> str:
     regex_pattern = "([0-9]{6})\\-[0-9]{7}"
-        # p = "[0-9]{6}\\-[0-9]{7}"
     return re.sub(regex_pattern, '<|rrn|>', input_text)
 
 
@@ -50,10 +40,3 @@ def reduce_emoticon(text: str, n=2):
     ex) ㅋㅋㅋㅋㅋㅋㅋ => ㅋㅋ
     """
     return emoticon_normalize(text, num_repeats=n)
-
-
-def strip_html_tags(text):
-    """remove html tags from text"""
-    soup = BeautifulSoup(text, "html.parser")
-    stripped_text = soup.get_text(separator=" ")
-    return stripped_text.strip()
