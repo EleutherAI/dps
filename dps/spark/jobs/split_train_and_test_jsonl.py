177efe1e798285bf745d206243a1b4b1349094e4
Taekyoon <tgchoi03@gmail.com>
2022-05-18 21:23:14 +0900

    [python] feat: Add split train and test dataset
    - Add a job to split train and test jsonl dataset
    
    Ref: #16

--- dps/spark/jobs/split_train_and_test_jsonl.py
+++ dps/spark/jobs/split_train_and_test_jsonl.py
@@ -1,4 +1,4 @@
-from random import seed, random
+import random
 
 from pyspark import SparkContext
 from pyspark.rdd import RDD
@@ -9,10 +9,10 @@ from ..utils.io import read_line, to_json
 
 def split_jsonl(input_path, test_ratio=0.1, seed=1234):
     SPLIT_AS = 'split_as'
-    seed(seed)
+    random.seed(seed)
     
     def tag_train_or_val(data):
-        if random() < test_ratio:
+        if random.random() < test_ratio:
             data[SPLIT_AS] = 'val'
         else:
             data[SPLIT_AS] = 'train'
@@ -39,6 +39,6 @@ def split_jsonl(input_path, test_ratio=0.1, seed=1234):
             .saveAsTextFile(f'{input_path}_train')
         
         val_rdd \
-            .repartition(10) \
+            .repartition(1) \
             .flatMap(to_json) \
             .saveAsTextFile(f'{input_path}_val')
--- dps/spark/run.py
+++ dps/spark/run.py
@@ -2,9 +2,11 @@ import fire
 
 from .jobs.sample_jsonl import sample_jsonl
 from .jobs.common_preprocess_jsonl import preprocess
+from .jobs.split_train_and_test_jsonl import split_jsonl
 
 def run():
     fire.Fire({
         'sample_jsonl': sample_jsonl,
         'common_preprocess_jsonl': preprocess,
+        'split_train_and_test_jsonl': split_jsonl
         })
