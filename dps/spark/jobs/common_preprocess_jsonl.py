0a515d2c6b2c31ce36a029441317bda6921e1304
Seungmoo Yang <ydaniel0826@gmail.com>
2022-05-23 12:02:41 +0900

    Add function for processing empty string

--- dps/spark/jobs/common_preprocess_jsonl.py
+++ dps/spark/jobs/common_preprocess_jsonl.py
@@ -1,9 +1,6 @@
 import os
-
-from pyspark import SparkContext
 from pyspark.rdd import RDD
 from pyspark.sql import DataFrame
-
 
 from ..spark_session import spark_session
 from ..utils.common_preprocess import (reduce_emoticon, 
@@ -12,6 +9,8 @@ from ..utils.common_preprocess import (reduce_emoticon,
                                        replace_phone_number, 
                                        remove_html_tags)
 from ..utils.io import to_json
+from functools import reduce
+from pyspark.sql.functions import col
 
 
 def preprocess_text(input_text: str):
@@ -38,11 +37,15 @@ def preprocess(input_dir: str, output_dir: str, n_dist: int=10, n_output: int=10
         raise ValueError('input_dir is not directory path')
     
     with spark_session(f'common_preprocess_jsonl_{input_dir}') as spark:
-        sc: SparkContext = spark.sparkContext
-        proc_df: DataFrame = spark.read.json(input_dir).repartition(n_dist) \
-            .select("text") \
-            .distinct()
-        
+        proc_df: DataFrame = spark.read.json(input_dir).repartition(n_dist)
+        columns = set(["text"])
+        filtered_null = map(lambda x: (col(x).isNotNull()) & (col(x) != ""), columns)
+        processed_null = reduce((lambda x, y: x & y), filtered_null)
+
+        proc_df = proc_df.select("text") \
+            .distinct() \
+            .filter(processed_null)
+            
         # convert to RDD and preprocess
         proc_rdd: RDD = proc_df.rdd \
             .repartition(n_dist) \
