547a9285788a428846e954debaf8873a5f982bad
daniel <ld4230@gmail.com>
2022-05-26 15:50:31 +0900

    remove useless type of statistics

--- tools/get_data_length_stat.py
+++ tools/get_data_length_stat.py
@@ -8,7 +8,7 @@ def define_argparser():
     
     parser.add_argument('--input_path', type=Path, required=True)
     parser.add_argument('--output_path', type=Path, required=True)
-    parser.add_argument('--tokenize', type=str, choices=['character', 'word'], default="word")
+    parser.add_argument('--tokenizer', type=str, choices=['character', 'word'], default="word")
     
     return parser.parse_args()
 
@@ -16,26 +16,26 @@ def data_stats_to_excel(config):
     """
     Args:
         config : 
-        --input_path (str): the path of train dataset for gpt.
-        --output_path (str): the path of data length statistics output.
-        --tokenize (str, optional): choose tokenizing options from ('character', 'word'). Defaults to 'word'.
+        --input_path (str): specify the path and filename of dataset to analyse.
+        --output_path (str): specify the path and filename for data length statistics output.
+        --tokenizer (str, optional): choose tokenizer options from ('character', 'word'). Defaults to 'word'.
 
     Returns:
         data length statistics excel file (.xlsx) : 
             It includes various length statistics.
-            The rows take name of data types and the columns take type of statistics.
+            The rows take name of data types (data sources) and the columns take type of statistics.
     """
     
-    # load data file
+    # load data
     data = pd.read_excel(config.input_path, index_col=0)
     
-    # make output file
+    # make output
     df = pd.DataFrame(index=data.type.unique(),
-                      columns=["nums", "min", "max", "mean", "median", "percentile_25", "percentile_75", "std", "qmarks", "fullstop", "alphabet_first", "numbers"])
+                      columns=["nums", "min", "max", "mean", "median", "percentile_25", "percentile_75", "std"])
     
     for type_name, type_data in data.groupby(['type']):
         type_data['text'] = type_data['text'].apply(str)
-        if config.tokenize == 'word':
+        if config.tokenizer == 'word':
             each_type_length = type_data['text'].apply(lambda x: len(x.split(' ')))
         else:
             each_type_length = type_data['text'].apply(len)
@@ -50,14 +50,7 @@ def data_stats_to_excel(config):
         percentile_75_length = round(np.percentile(each_type_length, 75), 2)
         std_of_length = round(np.std(each_type_length), 2)
         
-        percentage_of_qmarks = round(np.mean(type_data['text'].apply(lambda x: '?' in x)), 4)
-        percentage_of_fullstop = round(np.mean(type_data['text'].apply(lambda x: '.' in x)), 4)
-        percentage_of_alphabet_first = round(np.mean(type_data['text'].apply(lambda x: x[0].encode().isalpha())), 4)
-        percentage_of_numbers = round(np.mean(type_data['text'].apply(lambda x: max([y.isdigit() for y in x]))), 4)
-        
-        df.loc[type_name, :] = nb_data, \
-            min_length, max_length, mean_length, median_length, percentile_25_length, percentile_75_length, std_of_length, \
-            percentage_of_qmarks, percentage_of_fullstop, percentage_of_alphabet_first, percentage_of_numbers
+        df.loc[type_name, :] = nb_data, min_length, max_length, mean_length, median_length, percentile_25_length, percentile_75_length, std_of_length
     
     df.to_excel(config.output_path)
 
