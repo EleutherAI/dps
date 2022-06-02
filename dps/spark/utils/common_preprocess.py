423ec1dac25101f5ff7af2c372713be98c923400
Ronalmoo <ydaniel0826@gmail.com>
2022-06-03 00:07:16 +0900

    Update method

--- dps/spark/utils/common_preprocess.py
+++ dps/spark/utils/common_preprocess.py
@@ -34,13 +34,17 @@ def replace_rrn(input_text: str) -> str:
     return re.sub(regex_pattern, '<|rrn|>', input_text)
 
 
-def replace_bank_account(input_text: str) -> str:
-    pass
-
-
 def replace_creditcard(input_text: str) -> str:
-    regex_pattern = "(5[1-5]\d{14})|(4\d{12})(\d{3}?)|3[47]\d{13}|(6011\d{12})"
+    regex_pattern = "(5[1-5]\d{14})|(4\d{12})(\d{3}?)|3[47]\d{13}|(6011\d{12})|([0-9]{4})\\-([0-9]{4})\\-([0-9]{4})\\-([0-9]{4})"
     return re.sub(regex_pattern, '<|crd|>', input_text)
+
+
+def replace_bank_account(input_text: str) -> str:
+    regex_pattern = "^(\d{1,})(-(\d{1,})){1,}|([0-9]\d{11})|([0-9]\d{12})|([0-9]\d{13})|([0-9]\d{14})|([0-9]{3,4}\\-[0-9]{3,4}\\-[0-9]{5,6})|([0-9]{3}\\-[0-9]{2,4}\\-[0-9]{4,6}\\-[0-9]{1,2})|([0-9]{3}\\-[0-9]{6}\\-[0-9]{2,4}\\-[0-9]{3})"
+    # regex_pattern = re.compile("(\d{2,6}[ -]-?\d{2,6}[ -]-?\d{2,6}[ -]-?\d{2,6})")
+    # return re.sub(regex_pattern, '<|acc|>', input_text)
+    # if regex_pattern.search(input_text):
+    return re.sub(regex_pattern, '<|acc|>', input_text)
 
 
 def reduce_emoticon(text: str, n=2):
@@ -49,3 +53,8 @@ def reduce_emoticon(text: str, n=2):
     ex) ㅋㅋㅋㅋㅋㅋㅋ => ㅋㅋ
     """
     return emoticon_normalize(text, num_repeats=n)
+
+
+if __name__ == "__main__":
+    print(replace_bank_account("제 통장 번호는 110-274-742823입니다."))
+    
