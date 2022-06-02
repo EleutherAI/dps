53f5bf663ed3b41d50ac8e221175365d0d3ceec9
Ronalmoo <ydaniel0826@gmail.com>
2022-06-02 09:08:47 +0900

    Add func for replace creditcard in formation

--- dps/spark/utils/common_preprocess.py
+++ dps/spark/utils/common_preprocess.py
@@ -34,6 +34,15 @@ def replace_rrn(input_text: str) -> str:
     return re.sub(regex_pattern, '<|rrn|>', input_text)
 
 
+def replace_bank_account(input_text: str) -> str:
+    pass
+
+
+def replace_creditcard(input_text: str) -> str:
+    regex_pattern = "(5[1-5]\d{14})|(4\d{12})(\d{3}?)|3[47]\d{13}|(6011\d{12})"
+    return re.sub(regex_pattern, '<|crd|>', input_text)
+
+
 def reduce_emoticon(text: str, n=2):
     """
     Function that reduces repeating Korean characters
