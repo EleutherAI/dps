491b379e39bca5dc6ed2c91585273b89378fcd17
Taekyoon <tgchoi03@gmail.com>
2022-06-05 18:51:23 +0900

    Add scripts to run hadoop cluster (#25)
    - Add spark-submit script
    - Add dps to run spark-submit
    
    Ref: #20

--- dps/spark/jobs/common_preprocess_jsonl.py
+++ dps/spark/jobs/common_preprocess_jsonl.py
@@ -5,7 +5,7 @@ from pyspark.rdd import RDD
 from pyspark.sql import DataFrame
 
 
-from ..spark_session import spark_session
+from ..spark_session import spark_session, spark_session_for_cluster
 from ..utils.common_preprocess import (reduce_emoticon, 
                                        replace_rrn, 
                                        remove_whitespace, 
@@ -33,19 +33,22 @@ def preprocess_text(input_text: str):
     return processed_text
 
 
-def preprocess(input_dir: str, output_dir: str, n_dist: int=10, n_output: int=10):
-    if not os.path.isdir(input_dir):
+def preprocess(input_dir: str, output_dir: str, n_dist: int=10, n_output: int=10, is_cluster=False):
+    if (not is_cluster) and (not os.path.isdir(input_dir)):
         raise ValueError('input_dir is not directory path')
     
-    with spark_session(f'common_preprocess_jsonl_{input_dir}') as spark:
+    session_fn = spark_session_for_cluster if is_cluster else spark_session
+
+    with session_fn(f'common_preprocess_jsonl_{input_dir}') as spark:
         sc: SparkContext = spark.sparkContext
         proc_df: DataFrame = spark.read.json(input_dir).repartition(n_dist) \
             .select("text") \
-            .distinct()
+            .distinct() \
         
         # convert to RDD and preprocess
         proc_rdd: RDD = proc_df.rdd \
             .repartition(n_dist) \
+            .filter(lambda x: x["text"] != "") \
             .map(lambda x: dict(text=preprocess_text(x["text"])))
             
         proc_rdd.flatMap(to_json).repartition(n_output).saveAsTextFile(output_dir)
