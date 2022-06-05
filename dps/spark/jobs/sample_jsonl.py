9869ad5a3cb32539d785e50c35c3e62846837eca
Taekyoon Choi <tgchoi03@gmail.com>
2022-06-05 18:31:41 +0900

    change sample jsonl work logics

--- dps/spark/jobs/sample_jsonl.py
+++ dps/spark/jobs/sample_jsonl.py
@@ -3,25 +3,26 @@ import yaml
 from pyspark import SparkContext
 from pyspark.rdd import RDD
 
-from ..spark_session import spark_session
+from ..spark_session import spark_session, spark_session_for_cluster
 from ..utils.io import read_line, to_json
 
 
-def sample_jsonl(config_path):
-    with open(config_path) as f:
-        conf = yaml.load(f, Loader=yaml.FullLoader)
-    
-    input_paths = ','.join([f'{conf["base_dir"]}/{t}' for t in conf["targets"]])
+def sample_jsonl(input_dir: str, output_dir: str, n_dist: int=10, n_output: int=10, 
+                 ratio: float=0.1, seed: int=1234, is_cluster=False):
+    if (not is_cluster) and (not os.path.isdir(input_dir)):
+        raise ValueError('input_dir is not directory path')
 
-    with spark_session(f'sample_jsonl_size_{conf["sample_ratio"]}_seed_{conf["seed"]}') as spark:
+    session_fn = spark_session_for_cluster if is_cluster else spark_session
+
+    with session_fn(f'sample_jsonl_size_{ratio}_seed_{seed}') as spark:
         sc: SparkContext = spark.sparkContext
-
-        proc_rdd: RDD = sc.textFile(input_paths) \
-            .repartition(10) \
+        proc_rdd: RDD = sc.textFile(input_dir) \
+            .repartition(n_dist) \
             .flatMap(read_line) \
-            .sample(False, conf["sample_ratio"], conf["seed"])
+            .sample(False, ratio, seed)
             
         proc_rdd \
-            .repartition(1) \
+            .repartition(n_output) \
             .flatMap(to_json) \
-            .saveAsTextFile(conf["output_dir"])
+            .saveAsTextFile(output_dir)
+
