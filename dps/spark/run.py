8755dff2eb632e7fa22f78ac34643e083faac004
Taekyoon <tgchoi03@gmail.com>
2022-11-27 16:18:13 +0900

    Update run.py

--- dps/spark/run.py
+++ dps/spark/run.py
@@ -1,7 +1,7 @@
 import fire
 
 from .jobs.sample_jsonl import sample_jsonl
-from .jobs.build_news_paper_data import build_news_paper_data
+from .jobs.build_news_paper_data import build_korean_modu_news_paper_data
 from .jobs.massivetext_filter_jsonl import massivetext_filter_jsonl
 from .jobs.common_preprocess_jsonl import preprocess
 
@@ -10,5 +10,5 @@ def run():
         'sample_jsonl': sample_jsonl,
         'common_preprocess_jsonl': preprocess,
         'massivetext_quality_filtering': massivetext_filter_jsonl,
-        'build_news_paper_data': build_news_paper_data,
+        'build_news_paper_data': build_korean_modu_news_paper_data,
         })
