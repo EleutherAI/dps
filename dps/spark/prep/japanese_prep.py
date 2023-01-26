93b0a213b5a636133138d4bdd16e2aa8a59332c0
fujiki <fujiki.nakamura@gmail.com>
2023-01-26 18:37:24 +0900

    add several Japanese bad words and bad_words_filter for Japanese

--- dps/spark/prep/japanese_prep.py
+++ dps/spark/prep/japanese_prep.py
@@ -2,10 +2,18 @@ import re
 import sys
 
 from dps.spark.utils.japanese_utils import (
-    JAPANESE_CHARS
+    JAPANESE_CHARS,
+    BAD_WORDS,
 )
 
 
 def japanese_word_ratio_filter(text, japanese_word_ratio):
     return japanese_word_ratio <= len(re.findall(f'[{JAPANESE_CHARS}]', text))\
          / len(re.sub("[ \r\n\t\f\v", "", text)) + 1e-12
+
+
+def bad_words_filter(text):
+    for bad_word in BAD_WORDS:
+        if bad_word in text:
+            return False
+    return True
