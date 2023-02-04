54f73b4874f9ad80f95c4ed1bb6daf05f82e286c
fujiki <fujiki.nakamura@gmail.com>
2023-02-04 18:57:14 +0900

    fix the regular expression syntax

--- dps/spark/prep/japanese_prep.py
+++ dps/spark/prep/japanese_prep.py
@@ -21,8 +21,8 @@ def word_tokenize(text):
 
 
 def japanese_word_ratio_filter(text, japanese_word_ratio):
-    return japanese_word_ratio <= len(re.findall(f'[{JAPANESE_CHARS}]', text))\
-         / len(re.sub("[ \r\n\t\f\v", "", text)) + 1e-12
+    return japanese_word_ratio <= len(re.findall(f'[{JAPANESE_CHARS}]', text)) / (
+        len(re.sub("[ \r\n\t\f\v]", "", text)) + 1e-12)
 
 
 def japanese_bad_words_filter(text):
