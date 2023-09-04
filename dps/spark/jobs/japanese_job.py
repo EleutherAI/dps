619e48c26f0347268a2b35be93ff08c5b048d1cc
Paul McCann <132827365+polm-stability@users.noreply.github.com>
2023-09-04 18:18:23 +0900

    Update dps/spark/jobs/japanese_job.py
    Co-authored-by: Naoki Orii <mrorii@gmail.com>

--- dps/spark/jobs/japanese_job.py
+++ dps/spark/jobs/japanese_job.py
@@ -63,7 +63,7 @@ def japanese_job(config_path: str):
             return (japanese_bad_words_filter(text) and
                    doc_len_filter(text, conf["min_doc_len"], conf["max_doc_len"]) and
                    japanese_mean_word_len_filter(text, conf["min_mean_word_len"], conf["max_mean_word_len"]) and
-                   japanese_symbol_to_word_ratio_filter(text, conf["symbol_to_word_ration"]) and
+                   japanese_symbol_to_word_ratio_filter(text, conf["symbol_to_word_ratio"]) and
                    bullet_ellipsis_filter(text, config["bullet_point_ratio"], conf["ellipsis_ratio"]) and
                    japanese_word_ratio_filter(text, config["japanese_word_ratio"]) and
                    japanese_frequent_char_existence_filter(text, conf["freq_char_cnt"]) and
